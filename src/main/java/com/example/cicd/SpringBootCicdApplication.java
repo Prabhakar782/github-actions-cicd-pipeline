c1ed135d2d4a05f6de7eebac3e46d6720775afe3
prabhakar <prabhakarvajja783@gmail.com>
2026-02-09 17:16:02 +0530

    commit

--- src/main/java/com/example/cicd/SpringBootCicdApplication.java
+++ src/main/java/com/example/cicd/SpringBootCicdApplication.java
@@ -18,6 +18,6 @@ public class SpringBootCicdApplication {
 	@GetMapping("/getMessage")
 	public String getMessage() {
 		int age = 21;
-		return age > 19 ? "you age is eligible for vote" : "you are not eligible for vote";
+		return age > 19 ? "your age is eligible for vote" : "you are not eligible for vote";
 	}
 }
